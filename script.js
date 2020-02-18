1ee5f9fd99fa268e222f4a1050f717ae90b46af8
Jacqueline Cameron <jskye429@mail.com>
2020-02-17 19:14:57 -0600

    building password

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
 
-
- var upperCase = ["A", "B", "C", "D", "E", "F","G", "H", "I", "J","K", "L","M", "N", "O", "P", "Q", "R", "S", "T","U", "P", "Q", "R", "S", "T","U","V","W","X","Y","Z"]
- var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","q","r","s","t","u","v","w","x","y","z"]
- var numbers = ["1","2","3","4","5","6","7","8","9","0"]
- var specials = ["!","@","#","$","%","^","&","*","(",")","-","+"]
-
+var upperCase = ["A", "B", "C", "D", "E", "F","G", "H", "I", "J","K", "L","M", "N", "O", "P", "Q", "R", "S", "T","U", "P", "Q", "R", "S", "T","U","V","W","X","Y","Z"]
+var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","q","r","s","t","u","v","w","x","y","z"]
+var numbers = ["1","2","3","4","5","6","7","8","9","0"]
+var specials = ["!","@","#","$","%","^","&","*","(",")","-","+"]
+var MasterArray = [];
  
 
  var upperCase = document.getElementById("upperCase");
@@ -12,58 +13,38 @@
  var numbers = document.getElementById("numbers");
  var specials = document.getElementById("specials");
  var submitButton = document.getElementById("submitButton");
- var MasterArray = document.getElementById("MasterArray");
 
+  
+submitButton.addEventListener("click", manifest); {
 
-submitButton.addEventListener("click", function() {
+}
+
+function manifest() {
 
 if (upperCase.checked === true){ 
-    MasterArray += upperCase
+    MasterArray== MasterArray + upperCase
     };
 if (lowerCase.checked === true){
-    MasterArray += lowerCase
+    MasterArray== MasterArray + lowerCase
     };
 if (numbers.checked === true) {
-    MasterArray += numbers
+    MasterArray== MasterArray + numbers
+    
     };
-if (specials.checked == true) {
-    MasterArray += specials
-    };
-    console.log(MasterArray)    
-});
+if (specials.checked === true) {
+    MasterArray== MasterArray + specials
+    
 
-
-
-function manifest(){
-    MasterArray
-};
-
-
-
-
-
-
-
-
-
+  }};
+  console.log(MasterArray.value)
 
 
 
 
 
  
- 
- 
-//  <!-- user selects their preference-->
- 
-//  <!-- 26 leters, 10 digits, #of special characters, utilizing capital letters letter option becomes 52-->
- 
-//  <!-- a password then generates and displays to the user-->
- 
-//  <!-- # array to store data should be a way to hold alaphabet easily, the array loops 8 times for 8 characters-->
- 
-//  <!-- if then else? to ask options of user-->
- 
-//  <!-- output will be after 8 loops  -->
 
 
+
+// these last 3 characters close document.ready
+ }); 
